b887a98cff8e41a5396297916f6b40eeeee84045
sarfraaz1428 <sarfraaz14282007111@gmail.com>
2026-09-05 22:24:35 +0530

    Pattern13

---
+++ Pattern13.java
@@ -0,0 +1,24 @@
+public class Pattern13 {
+  public static void main(String[] args) {
+    int n = 5;
+    for (int i = 1; i <= n; i++) {
+
+      // space
+      for (int j = 1; j <= n - i; j++) {
+        System.out.print(" ");
+      }
+
+      // first half Number
+      for (int j = i; j >= 1; j--) {
+        System.out.print(j);
+
+      }
+      // 2nd half number
+      for (int j = 2; j <= i; j++) {
+        System.out.print(j);
+      }
+      System.out.println();
+    }
+  }
+
+}
